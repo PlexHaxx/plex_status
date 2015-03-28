d3b03dba7df9f1d2f8983cf3aba8d767414c843e
Brandon Smith <blsmit5728@gmail.com>
2015-03-28 10:15:40 -0400

    Added place to keep pushbullet key so not everyone has it.

--- get_plex_status.py
+++ get_plex_status.py
@@ -1,16 +1,28 @@
-import xml.etree.ElementTree as ET
-import wget
 import urllib2
 import xmltodict
+from pushbullet import Pushbullet
 
+# read my push bullet key from a file, so we can commit this.
+f = open("pb_key.txt","r")
+x = f.readline()
+y = x.strip('\n')
+PB_KEY = y
+f.close()
+
+# open a temp file to store the xml.
 f = open("temp.xml","wr")
 response = urllib2.urlopen('http://192.168.1.24:32400/status/sessions/')
 xml = response.read()
 f.write(xml)
 
+# Parse out the XML
 obj = xmltodict.parse(xml)
 
+# get the number of items currently playing.
 num_playing = int(obj['MediaContainer']['@size'])
+
+# if there is more than one, we have to index the video key. 
+# so we if on these 2 cases.
 if num_playing > 1:
     print num_playing
     for i in range(num_playing - 1):
@@ -26,7 +38,10 @@ if num_playing > 1:
             duration = int(obj['MediaContainer']['Video'][i]['@duration'])
             current = int(obj['MediaContainer']['Video'][i]['@viewOffset'])
         progress = int((current/duration)*100)
-        print username + " is Playing \"" + playing  + "\" on Plex and is at " + str(progress) + " percent done"
+        string = username + " is Playing \"" + playing  + "\" on Plex and is at " + str(progress) + " percent done"
+        print string
+        pb = Pushbullet(PB_KEY)
+        push = pb.push_note("[PLEX]",string)
 elif num_playing == 1 :
     media_type = str(obj['MediaContainer']['Video']['@type'])
     if media_type == "movie":
@@ -41,11 +56,10 @@ elif num_playing == 1 :
         duration = float(obj['MediaContainer']['Video']['@duration'])
         current = float(obj['MediaContainer']['Video']['@viewOffset'])
     progress = float((current/duration)*100)
-    print username + " is Playing \"" + playing  + "\" on Plex and is at " + str(progress) + " percent done"
+    string = username + " is Playing \"" + playing  + "\" on Plex and is at " + str(progress) + " percent done"
+    print string
+    pb = Pushbullet(PB_KEY)
+    push = pb.push_note("[PLEX]",string)
 else:
     print "No one is watching anything"
-
-
-
-
 
