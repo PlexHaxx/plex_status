868f5b3f06158abd746e18183543a142b24387ba
Brandon Smith <blsmit5728@gmail.com>
2015-03-31 07:59:59 -0400

    Fixed error that was causing this not to run.

--- get_plex_status.py
+++ get_plex_status.py
@@ -2,12 +2,30 @@ import urllib2
 import xmltodict
 from pushbullet import Pushbullet
 
+def start_html(html):
+    html.write("<html><head><title>[UARNIUM] Plex Status</title></head><body>")
+    
+def finish_html(html):
+    html.write("</body></html>")
+
+def write_html_entry(html,title,message):
+    html.write("<h2>")
+    html.write(title)
+    html.write("</h2><p>")
+    html.write(message)
+    html.write("</p>")
+
+
 # read my push bullet key from a file, so we can commit this.
 f = open("/home/bsmith/repos/plex_status/pb_key.txt","r")
 x = f.readline()
 y = x.strip('\n')
 PB_KEY = y
 f.close()
+
+db_file = open("/home/bsmith/Dropbox/Public/plex_status/status.txt","w")
+html = open("/home/bsmith/Dropbox/Public/plex_status/plex_status.html","w")
+start_html(html)
 
 # open a temp file to store the xml.
 f = open("/home/bsmith/repos/plex_status/temp.xml","wr")
@@ -30,19 +48,23 @@ if num_playing > 1:
         if media_type == "movie":
             username = str(obj['MediaContainer']['Video'][i]['User']['@title'])
             playing = str(obj['MediaContainer']['Video'][i]['@title'])
-            duration = int(obj['MediaContainer']['Video'][i]['@duration'])
-            current = int(obj['MediaContainer']['Video'][i]['@viewOffset'])
+            duration = float(obj['MediaContainer']['Video'][i]['@duration'])
+            current = float(obj['MediaContainer']['Video'][i]['@viewOffset'])
         else:
             username = str(obj['MediaContainer']['Video'][i]['User']['@title'])
             playing = str(obj['MediaContainer']['Video'][i]['@grandparentTitle'])
-            duration = int(obj['MediaContainer']['Video'][i]['@duration'])
-            current = int(obj['MediaContainer']['Video'][i]['@viewOffset'])
-        progress = int((current/duration)*100)
+            duration = float(obj['MediaContainer']['Video'][i]['@duration'])
+            current = float(obj['MediaContainer']['Video'][i]['@viewOffset'])
+        progress = float((current/duration)*100)
+        progress = round(progress,2)
         message = username + " is Playing \"" + playing  + "\" on Plex and is at " + str(progress) + " percent done"
         title = "[PLEX] " + username + " is Watching"
         print message
         pb = Pushbullet(PB_KEY)
         push = pb.push_note(title,message)
+        db_file.write(title)
+        db_file.write(message)
+        write_html_entry(html,title,message)
 elif num_playing == 1 :
     media_type = str(obj['MediaContainer']['Video']['@type'])
     if media_type == "movie":
@@ -50,18 +72,27 @@ elif num_playing == 1 :
         playing = str(obj['MediaContainer']['Video']['@title'])
         duration = float(obj['MediaContainer']['Video']['@duration'])
         current = float(obj['MediaContainer']['Video']['@viewOffset'])
-        print duration,current        
     else:
         username = str(obj['MediaContainer']['Video']['User']['@title'])
         playing = str(obj['MediaContainer']['Video']['@grandparentTitle'])
         duration = float(obj['MediaContainer']['Video']['@duration'])
         current = float(obj['MediaContainer']['Video']['@viewOffset'])
     progress = float((current/duration)*100)
+    progress = round(progress,2)
     message = username + " is Playing \"" + playing  + "\" on Plex and is at " + str(progress) + " percent done"
     title = "[PLEX] " + username + " is Watching"
-    print string
+    print message
     pb = Pushbullet(PB_KEY)
     push = pb.push_note(title,message)
+    db_file.write(title)
+    db_file.write(message)
+    write_html_entry(html,title,message)
 else:
     print "No one is watching anything"
+    db_file.write("No one is watching anything")
+    write_html_entry(html,"No Activity","Sad Plex, no activity right now")
 
+f.close()
+db_file.close()    
+finish_html(html)
+html.close()
